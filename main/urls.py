1c7fcd4e433d18b785734691db7b9391f06cbf0a
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2026-02-12 18:47:35 +0600

    Welcome page added! And added new tokens

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('token/<int:token_id>/', views.post_list, name='post_list_by_token'),
-    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('', views.welcome, name='welcome'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/token/<int:token_id>/', views.post_list, name='post_list_by_token'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Token
 from django.db.models import Q
 
+
+def welcome(request):
+    tokens = Token.objects.all()
+    posts_count = Post.objects.count()
+
+    return render(request, 'posts/welcome.html', {
+        'tokens': tokens,
+        'posts_count': posts_count,
+    })
+
+
 def post_list(request, token_id=None):
     tokens = Token.objects.all()
     query = request.GET.get('q', '')
@@ -24,7 +35,7 @@ def post_list(request, token_id=None):
         'query': query,
     })
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
-
